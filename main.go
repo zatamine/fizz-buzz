a7c9430e02371b993cf582cedb1208d6d323e812
Amine HADDAD <264999+zatamine@users.noreply.github.com>
2022-03-15 04:36:41 +0100

    Remove useless test

--- main.go
+++ main.go
@@ -1,24 +1,16 @@
 package main
 
 import (
-	"io"
 	"log"
-	"os"
 )
 
 func main() {
-	os.Exit(realMain(os.Stdout))
-}
-func realMain(out io.Writer) int {
-
 	storage := NewInmemory()
 	service := NewfizzBuzzService(storage)
 	controller := NewController(service)
 	router := NewGinRouter(controller)
 
 	if err := router.Serve(":8080"); err != nil {
-		log.Printf("Cannot run HTTP server, %s", err)
-		return 1
+		log.Fatalf("Cannot run HTTP server, %s", err)
 	}
-	return 0
 }
